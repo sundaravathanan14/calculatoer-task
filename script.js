c6f126940faf4d7282ecd795d45ad8c9b2cc644f
sundaravathanan14 <sundar.aero11@gmail.com>
2023-05-09 07:52:46 +0530

    added key down event

--- script.js
+++ script.js
@@ -7,13 +7,33 @@ var disply = document.createElement('div');
 disply.setAttribute("class","disply");
 var displayValue=document.createElement('input');
 displayValue.setAttribute("type","text");
-displayValue.setAttribute("name","display")
+displayValue.setAttribute("name","display");
+displayValue.setAttribute("id","display");
 disply.append(displayValue);
 form.append(disply);
 calculator.append(form);
 container.append(calculator);
 document.body.append(container);
 createCalc();
+
+
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    var keyCode = event.keyCode;
+    if (keyCode === 8 || keyCode === 46 || (keyCode >= 37 && keyCode <= 40) || (keyCode >= 48 && keyCode <= 57)){
+        document.getElementById("display").value +=name;
+       
+    } else{
+        alert("please enter only numbers");
+        event.preventDefault();
+    }
+    
+  }, false);
+
+
+ 
+
+
 function createCalc(){
     var firstRow = ["AC","DE",".","/"];
     var secondRow=["7","8","9","*"];
@@ -26,7 +46,7 @@ for(let i=0;i<5;i++){
         form.append(div);
         for(let j=0;j<firstRow.length;j++){
             if(firstRow[j] !="AC" && firstRow[j] !="DE"){
-            div.innerHTML+=`<input type="button" value="${firstRow[j]}" onclick="display.value += '${firstRow[j]}' ">`;
+            div.innerHTML+=`<input type="button" value="${firstRow[j]}"  onclick="display.value += '${firstRow[j]}' ">`;
             } else if(firstRow[j] =="AC"){
                 div.innerHTML+=`<input type="button" value="AC" onclick="display.value = '' " >`;
             }else{
